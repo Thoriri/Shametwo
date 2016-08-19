74e19671ffebb57bfd8dd4e21a2c639bcdf18374
occasionales <thorirmar1@gmail.com>
2016-08-19 00:55:25 +0000

    Initial commit

---
+++ app/src/main/java/com/example/thorir/shame/FirebaseInstanceIDService.java
@@ -0,0 +1,31 @@
+package com.example.thorir.shame;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+
+import com.google.firebase.iid.FirebaseInstanceId;
+import com.google.firebase.iid.FirebaseInstanceIdService;
+
+import java.io.IOException;
+
+import okhttp3.FormBody;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.RequestBody;
+
+/**
+ * Created by Thorir on 17.8.2016.
+ */
+public class FirebaseInstanceIDService extends FirebaseInstanceIdService {
+    @Override
+    public void onTokenRefresh() {
+
+        String recent_token = FirebaseInstanceId.getInstance().getToken();
+        SharedPreferences sharedPreferences = getApplicationContext().getSharedPreferences(getString(R.string.FCM_PREF), Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+        editor.putString(getString(R.string.FCM_TOKEN), recent_token);
+        editor.commit();
+    }
+
+
+}
